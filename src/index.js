65adabcf60ade68f189b4e465305902f7dc0f01d
Manpvph13976 <manpvph13976@fpt.edu.vn>
2022-10-19 20:11:58 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
-const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-const path = require("path");
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({
-    noCors: true,
-});
-server.db = router.db;
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+const cors = require('cors')
+const path = require('path')
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults({
+//   noCors: true,
+// })
+server.db = router.db
 
 const rules = auth.rewriter({
-    // Permission rules
-    // users: 600,
-    // messages: 640
-});
+  // Permission rules
+  // users: 600,
+  // messages: 640
+})
 
 // You must apply the middlewares in the following order
-server.use(rules);
-server.use(auth);
-
-server.use(middlewares);
+server.use(cors())
+server.use(rules)
+server.use(auth)
 server.use((req, res, next) => {
-    if (req.method === "POST") {
-        req.body.createAt = Date.now();
-        req.body.updateAt = Date.now();
-    } else if (req.method === "PATCH") {
-        req.body.updateAt = Date.now();
-    }
-    next();
-});
+//     res.setHeader('Access-Control-Allow-Origin: https://assignmentangular.vercel.app','Access-Control-Allow-Methods: GET, POST, PUT')
+//     res.setHeader('Access-Control-Allow-Headers', '*')
+   res.setHeader('Access-Control-Allow-Origin', 'https://fe-asm-manpvph13976.vercel.app/')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  if (req.method === 'POST') {
+    req.body.createAt = Date.now()
+    req.body.updateAt = Date.now()
+  } else if (req.method === 'PATCH') {
+    req.body.updateAt = Date.now()
+  }
+  next()
+})
 
-server.use("/api", router);
+server.use('/api', router)
 server.listen(8080, () => {
-    console.log("JSON Server is running");
-});
+  console.log('JSON Server is running')
+})
